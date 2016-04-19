172d8408ef9d94caf755e40233e32740304d65aa
Your Name <you@example.com>
2016-04-19 16:08:08 +0800

    Yulp API

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,22 +2,38 @@
 'use strict';
 angular.module('yulpApp')
 .controller('FeedCtrl', ['YelpAPI', FeedCtrl])
-.controller('FeedDetailsCtrl', ['$stateParams', 'YelpAPI', FeedDetailsCtrl]);
+.controller('FeedDetailsCtrl', ['$stateParams', 'YelpAPI', FeedDetailsCtrl])
+.controller('SearchCtrl', ['YelpAPI', '$scope', SearchCtrl]);
 function FeedCtrl(YelpAPI) {
 var vm = this; // view model (vm)
-YelpAPI.getData().then(function(res) {
-vm.total = res.data.total;
-vm.businesses = res.data.businesses;
+// YelpAPI.getData().then(function(res) {
+// vm.total = res.data.total;
+// vm.businesses = res.data.businesses;
+YelpAPI.getData(function(data) {
+vm.total = data.total;
+vm.businesses = data.businesses;
 console.log(vm.businesses);
 });
+}
+
+function SearchCtrl(YelpAPI, $scope){
+	var vm = this;
+	vm.toSearch = '';
+	vm. search = function(){
+		YelpAPI.searchData(vm.toSearch, function(data) {
+			vm.businesses =  data.businesses;
+		});
+	}
 }
 
 function FeedDetailsCtrl($stateParams, YelpAPI) {
 	var vm = this;
 	vm.businessId = $stateParams.businessId;
 	console.log(vm.businessId);
-	YelpAPI.getData().then(function(res) {
-vm.businesses = res.data.businesses;
+// 	YelpAPI.getData().then(function(res) {
+// vm.businesses = res.data.businesses;
+YelpAPI.getData(function(data) {
+vm.businesses = data.businesses;
 vm.business = vm.businesses[0]; // temporary
 });
 }
--- www/js/services.js
+++ www/js/services.js
@@ -1,14 +1,64 @@
+// (function() {
+// 'use strict';
+// angular.module('yulpApp')
+// .factory('YelpAPI', ['$http', YelpAPI]);
+// //.factory('anotherService', [anotherService])
+// function YelpAPI($http) {
+// function getData() {
+// return $http.get('/js/data.json');
+// }
+// return {
+// getData: getData
+// };
+// }
+// })();
+
 (function() {
 'use strict';
 angular.module('yulpApp')
 .factory('YelpAPI', ['$http', YelpAPI]);
 //.factory('anotherService', [anotherService])
 function YelpAPI($http) {
-function getData() {
-return $http.get('/js/data.json');
+	function randomString(length, chars){
+		var result = '';
+		for(var i = length; i>0; --i){
+			result += chars[Math.round(Math.random() * (chars.length -1))];
+		}
+		return result;
+	}
+	function searchAPI(location, limit, callback) {
+		var method = 'GET';
+		var url = 'http://api.yelp.com/v2/search';
+		var params = {
+			location: location,
+			oauth_consumer_key: 'zUUv2AiQVTEMOxycV7gPtA',
+			oauth_token: 'XvijwN_OX7CY60tVb_Q-K19X8hwmkqdx',
+			oauth_signature_method: 'HMAC-SHA1',
+			oauth_timestamp: new Date().getTime(),
+			oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+			limit: limit,
+			cc: 'MY'
+		};
+		var consumerSecret = 'OOmOHdgXxPpe56WjSDn1kAl7iJw';
+		var tokenSecret = 'kkK3v70885ojtaiFM48pEsZfS6o';
+		var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encodeSignature:false});
+		params['oauth_signature'] = signature;
+
+		$http.get(url, { params: params}).then(function (res){
+			callback(res.data);
+		});
+	}
+
+
+function getData(callback) {
+	return searchAPI('Kuala Lumpur', 10, callback);
 }
+function searchData(toSearch, callback) {
+		return searchAPI(toSearch,10,callback);
+	}
 return {
-getData: getData
+getData: getData,
+searchData: searchData
 };
 }
 })();
